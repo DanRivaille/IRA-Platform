5def32b7fd35ca863c831fbd8e9573ee7a045e71
DanRivaille <ivan.santos.v@mail.pucv.cl>
2023-11-16 18:33:20 -0300

    Se crea la clase abstracta Algorithm

---
+++ src/algorithm/Algorithm.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class Algorithm(ABC):
+    """
+    Creates a new instance of the algorithm
+    """
+    @staticmethod
+    @abstractmethod
+    def create():
+        pass
+
+    """
+    Loads an algorithm instance using a file
+    """
+    @staticmethod
+    @abstractmethod
+    def load():
+        pass
+
+    """
+    Saves the current state of the algorithm
+    """
+    @staticmethod
+    @abstractmethod
+    def save():
+        pass
