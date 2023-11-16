6dfa7b9096cad1d4cb78eef885c807c48b680446
DanRivaille <ivan.santos.v@mail.pucv.cl>
2023-11-16 18:48:43 -0300

    Se crean los metodos abstractos train y test en MLModel

--- src/algorithm/ml_model/MLModel.py
+++ src/algorithm/ml_model/MLModel.py
@@ -1,7 +1,20 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from src.algorithm.Algorithm import Algorithm
 
 
 class MLModel(Algorithm, ABC):
-    pass
+
+    """
+    Runs the train process
+    """
+    @abstractmethod
+    def train(self):
+        pass
+
+    """
+    Runs the test process
+    """
+    @abstractmethod
+    def test(self):
+        pass
