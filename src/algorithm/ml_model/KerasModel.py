01b7c8e3dc97ed58c2b6af7f342123414f0e5a45
DanRivaille <ivan.santos.v@mail.pucv.cl>
2023-11-23 20:56:37 -0300

    Se comienza a implementar los modelos de Keras

---
+++ src/algorithm/ml_model/KerasModel.py
@@ -0,0 +1,86 @@
+from keras import Model
+from keras.models import load_model
+from keras.losses import mean_squared_error
+from keras.optimizers import Adam
+from tensorflow.python.framework.config import list_physical_devices
+from torch.utils.data import DataLoader
+from tensorflow import device
+from keras.callbacks import ReduceLROnPlateau
+
+from src.algorithm.ml_model.History import History
+from src.algorithm.ml_model.MLModel import MLModel
+from src.algorithm.ml_model.models.AEKeras import get_autoencoder_keras
+from src.config.ConfigParams import ConfigParams
+
+
+class KerasModel(MLModel):
+    def __init__(self, identifier, input_length: int, learning_rate: float):
+        super().__init__(identifier)
+
+        self.__optimizer = Adam(learning_rate=learning_rate)
+        self.__loss_function = mean_squared_error
+        self.__device = KerasModel.__get_device()
+
+        self.__reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9,
+                                             patience=4, min_lr=learning_rate / 100)
+
+        # instanciate a keras model
+        with device(self.__device):
+            self.model: Model = get_autoencoder_keras(input_length, self.__optimizer, self.__loss_function)
+
+    @staticmethod
+    def get_file_extension():
+        return '.keras'
+
+    @staticmethod
+    def __get_device():
+        if len(list_physical_devices('GPU')) > 0:
+            return '/device:GPU:0'
+        else:
+            return '/device:CPU:0'
+
+    @staticmethod
+    def load(config: ConfigParams, identifier: str, path: str):
+        keras_model = KerasModel.create(config, identifier)
+        keras_model.model = load_model(path)
+
+    @staticmethod
+    def create(config: ConfigParams, identifier: str):
+        sequences_length = config.get_params_dict('preprocess_params')['sequences_length']
+        learning_rate = config.get_params_dict('train_params')['learning_rate']
+        return KerasModel(identifier, sequences_length, learning_rate)
+
+    def save(self, config: ConfigParams, path: str):
+        self.model.save(path)
+
+    def train(self, config: ConfigParams, trainloader: DataLoader, validationloader: DataLoader | None) -> History:
+        num_epochs = config.get_params_dict('train_params')['num_epochs']
+        batch_size = config.get_params_dict('train_params')['batch_size']
+
+        with device(self.__device):
+            history = self.model.fit(
+                x=trainloader, y=trainloader,
+                validation_data=(validationloader, validationloader),
+                epochs=num_epochs,
+                batch_size=batch_size,
+                callbacks=[self.__reduce_lr],
+                verbose=1
+            )
+
+        history_dict = history.history
+        _, train_error_per_sample = self.predict(trainloader, return_per_sample=True)
+        return History(history_dict['loss'], history_dict['val_loss'], history_dict['lr'], train_error_per_sample)
+        pass
+
+    # TODO: Check this function
+    def test(self, config: ConfigParams, testloader: DataLoader, validationloader: DataLoader):
+        pass
+
+    def predict(self, dataloader: DataLoader, return_per_sample: bool = True, **kwargs) -> tuple:
+        sequences_predicted = self.model.predict(dataloader, verbose=0)
+        error_per_sample = mean_squared_error(dataloader, sequences_predicted).numpy()
+
+        if return_per_sample:
+            return error_per_sample
+        else:
+            return error_per_sample.mean()
---
+++ src/algorithm/ml_model/models/AEKeras.py
@@ -0,0 +1,23 @@
+from keras import Sequential, Model
+from keras.layers import Dense
+
+
+def get_autoencoder_keras(input_size: int, optimizer, loss_function, metric_list=None) -> Model:
+    if metric_list is None:
+        metric_list = []
+
+    encoding_dim = 128
+    model = Sequential()
+
+    # Encoder
+    model.add(Dense(256, activation='tanh', input_shape=(input_size,)))
+    model.add(Dense(encoding_dim, activation='tanh'))
+
+    # Decoder
+    model.add(Dense(256, activation='tanh'))
+
+    # Output layer
+    model.add(Dense(input_size, activation='sigmoid'))
+
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=metric_list)
+    return model
