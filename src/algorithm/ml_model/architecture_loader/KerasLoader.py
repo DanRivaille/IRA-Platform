e7dabf20a870b38f5b406c87058e898ba8c7e7de
DanRivaille <ivan.santos.v@mail.pucv.cl>
2023-11-26 19:24:50 -0300

    Se crea el modelo LSTM-AE en keras

--- src/algorithm/ml_model/architecture_loader/KerasLoader.py
+++ src/algorithm/ml_model/architecture_loader/KerasLoader.py
@@ -1,11 +1,29 @@
-from keras import Model
+from keras import Model, Sequential
+from keras.src.layers import Dense, LSTM
 
 from src.config.ConfigParams import ConfigParams
 
 
 class KerasLoader:
     def __init__(self, config_params: ConfigParams):
-        self.__config = config_params
+        self.__config: config_params = config_params
+        self.__topology: [dict] = self.__config.get_params_dict('network_params').get('topology')
 
     def load(self) -> Model:
+        model = Sequential()
+
+        for layer in self.__topology:
+            model.add(KerasLoader.__load_layer(layer))
+
+        return model
+
+    @staticmethod
+    def __load_layer(layer_info: dict):
         pass
+
+    @staticmethod
+    def __get_layer(layer_code: str):
+        if "dense_keras_layer" == layer_code:
+            return Dense
+        elif "lstm_keras_layer" == layer_code:
+            return LSTM
--- src/algorithm/ml_model/models/AEKeras.py
+++ src/algorithm/ml_model/models/AEKeras.py
@@ -21,3 +21,17 @@ def get_autoencoder_keras(input_size: int, optimizer, loss_function, metric_list
 
     model.compile(optimizer=optimizer, loss=loss_function, metrics=metric_list)
     return model
+
+
+def get_autoencoder_keras_modify(optimizer, loss_function, metric_list=None) -> Model:
+    model = Sequential()
+
+    # Encoder
+    model.add(Dense(256, activation='tanh', input_shape=(1000,)))
+    model.add(Dense(128, activation='tanh'))
+    model.add(Dense(256, activation='tanh'))
+    model.add(Dense(1000, activation='sigmoid'))
+
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=metric_list)
+    return model
+
