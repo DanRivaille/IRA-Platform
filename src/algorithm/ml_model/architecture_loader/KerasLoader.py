f1a5fe8aa279b3e5877fd0fa93824ad8eaa2be95
DanRivaille <ivan.santos.v@mail.pucv.cl>
2023-11-26 19:39:34 -0300

    Se implementa KerasLoader (todavia no se prueba)

--- src/algorithm/ml_model/architecture_loader/KerasLoader.py
+++ src/algorithm/ml_model/architecture_loader/KerasLoader.py
@@ -1,5 +1,5 @@
 from keras import Model, Sequential
-from keras.src.layers import Dense, LSTM
+from keras.src.layers import Dense, LSTM, RepeatVector, TimeDistributed
 
 from src.config.ConfigParams import ConfigParams
 
@@ -19,7 +19,15 @@ class KerasLoader:
 
     @staticmethod
     def __load_layer(layer_info: dict):
-        pass
+        layer = KerasLoader.__get_layer(layer_info.get('layer'))
+
+        if layer == 'time_distributed_keras_layer':
+            return layer(KerasLoader.__load_layer(layer_info['time_distributed_layer']))
+
+        layer_params = KerasLoader.__get_layer_params(layer_info)
+        n_neurons = layer_info.get('neurons')
+
+        return layer(n_neurons, **layer_params)
 
     @staticmethod
     def __get_layer(layer_code: str):
@@ -27,3 +35,23 @@ class KerasLoader:
             return Dense
         elif "lstm_keras_layer" == layer_code:
             return LSTM
+        elif "repeat_vector_keras_layer" == layer_code:
+            return RepeatVector
+        elif "time_distributed_keras_layer" == layer_code:
+            return TimeDistributed
+
+    @staticmethod
+    def __get_layer_params(layer_info: dict) -> dict:
+        layer_params = {}
+
+        KerasLoader.__add_layer_param(layer_info, layer_params, 'input_shape')
+        KerasLoader.__add_layer_param(layer_info, layer_params, 'activation_function')
+        KerasLoader.__add_layer_param(layer_info, layer_params, 'return_sequences')
+
+        return layer_params
+
+    @staticmethod
+    def __add_layer_param(layer_info: dict, layer_params: dict, keyparam_to_add: str) -> None:
+        param_to_add = layer_info.get(keyparam_to_add, None)
+        if param_to_add is not None:
+            layer_params[keyparam_to_add] = param_to_add
