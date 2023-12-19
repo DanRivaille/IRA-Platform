4fa8724a1fb554db7aada662404096aed8a425e9
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-19 19:26:35 -0300

    Se documenta el archivo utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,8 +4,12 @@ import numpy as np
 
 from src.config.CommonPath import CommonPath
 
-
 def stack_arrays(stacked_array: np.ndarray, new_array: np.ndarray) -> np.ndarray:
+    """
+    Stacks an array inside an array stack, if the array stack is empty then returns the new array.
+    @param stacked_array Current array stack (may be empty).
+    @param new_array A new array to enter the stack.
+    """
     if stacked_array.shape[0] != 0:
         return np.vstack((stacked_array, new_array))
     else:
@@ -13,5 +17,10 @@ def stack_arrays(stacked_array: np.ndarray, new_array: np.ndarray) -> np.ndarray
 
 
 def build_model_folderpath(model_identifier: str, config_identifier: str):
+    """
+    Builds a directory path for a model and its configuration based on the provided identifiers
+    @param model_identifier A string identifier for the current model.
+    @param config_identifier A string identifier of the current model configuration. 
+    """
     model_folder = f'{model_identifier}_cnf_{config_identifier}'
     return os.path.join(CommonPath.MODEL_PARAMETERS_FOLDER.value, model_folder)
