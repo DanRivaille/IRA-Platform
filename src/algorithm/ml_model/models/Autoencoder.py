696293bbdfbe8f07e8787e70efc29e41b187790b
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-20 17:47:47 -0300

    Se documenta el archivo Autoencoder.py

--- src/algorithm/ml_model/models/Autoencoder.py
+++ src/algorithm/ml_model/models/Autoencoder.py
@@ -2,7 +2,14 @@ from torch.nn import Module, Sequential, Linear, Tanh
 
 
 class Autoencoder(Module):
+    """
+    A class of an autoencoder model using PyTorch.
+    """
     def __init__(self, input_length):
+        """
+        Initializes an instance of Autoencoder.
+        @param input_length The size of each input and output layer entry.
+        """
         super().__init__()
         self.encoder = Sequential(
             Linear(input_length, 256),
@@ -19,6 +26,10 @@ class Autoencoder(Module):
         )
 
     def forward(self, x):
+        """
+        Performs the forward pass of the autoencoder.
+        @param x The input tensor to be encoded and then decoded.
+        """
         x = self.encoder(x)
         x = self.decoder(x)
         return x
