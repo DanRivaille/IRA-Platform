cee42ac06e50b31d3a660144512b6dd033c2e480
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-20 17:48:51 -0300

    Se documenta el archivo LSTM_AE_Keras.py

--- src/algorithm/ml_model/models/LSTM_AE_Keras.py
+++ src/algorithm/ml_model/models/LSTM_AE_Keras.py
@@ -3,6 +3,14 @@ from keras.src.layers import LSTM, RepeatVector, TimeDistributed, Dense
 
 
 def get_lstm_ae_keras(input_size: int, n_features: int, optimizer, loss_function: str, metric_list=None) -> Model:
+    """
+    Constructs an LSTM-Autoencoder model using Keras.
+    @param input_size The size of each input and output layer entry.
+    @param n_features The number of inputs for the input and output layer. 
+    @param optimizer The optimizer to be used during model compilation.
+    @param loss_function The loss function to be used during model compilation.
+    @param metric_list List of metrics to be monitored during the model training.
+    """
     if None is metric_list:
         metric_list = []
 
@@ -23,6 +31,11 @@ def get_lstm_ae_keras(input_size: int, n_features: int, optimizer, loss_function
 
 
 def get_lstm_ae_keras_modify(optimizer, loss_function: str) -> Model:
+    """
+    Constructs a modified version of an LSTM-Autoencoder model using Keras.
+    @param optimizer The optimizer to be used during model compilation.
+    @param loss_function The loss function to be used during model compilation.
+    """
     model = Sequential()
 
     model.add(LSTM(128, input_shape=(50, 1), return_sequences=False))
