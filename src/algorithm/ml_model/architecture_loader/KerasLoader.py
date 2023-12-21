7b6e40eab1eb2b57609cbf09b17188ca2947e42a
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-21 13:43:13 -0300

    Se documenta el archivo KerasLoader.py

--- src/algorithm/ml_model/architecture_loader/KerasLoader.py
+++ src/algorithm/ml_model/architecture_loader/KerasLoader.py
@@ -6,10 +6,20 @@ from src.config.ConfigParams import ConfigParams
 
 
 class KerasLoader:
+    """
+    A class responsible for loading a Keras model based on configuration parameters.
+    """
     def __init__(self, config_params: ConfigParams):
+        """
+        Initializes an instance of KerasLoader.
+        @param config_params A class for loading configuration parameters related to a model.
+        """
         self.config_params = config_params
 
     def load(self) -> Model:
+        """
+        Loads a Keras model with the layers specified in the provided configuration parameters.
+        """
         topology = self.config_params.get_params_dict('network_params').get('topology')
 
         model = Sequential()
@@ -24,6 +34,11 @@ class KerasLoader:
 
     @staticmethod
     def __load_compilation_params(config_params: ConfigParams) -> dict:
+        """
+        Loads a Keras model with compilation params specified in the provided configuration.
+        @param config_params A class for loading configuration parameters related to a model.
+        @return Dict: With the compilation params (optimizer and loss function).
+        """
         network_params = config_params.get_params_dict('network_params')
         optimizer = KerasLoader.__get_optimizer(network_params.get('optimizer'))
         learning_rate = config_params.get_params_dict('train_params')['learning_rate']
@@ -35,11 +50,19 @@ class KerasLoader:
 
     @staticmethod
     def __get_optimizer(optimizer_code: str):
+        """
+        Returns the optimizer class based on the provided optimizer code.
+        @param optimizer_code The code of the optimizer to return.
+        """
         if "adam" == optimizer_code:
             return Adam
 
     @staticmethod
     def __load_layer(layer_info: dict):
+        """
+        Loads and returns a Keras layer based on the provided layer information.
+        @param layer_info A dictionary of the layers to load.
+        """
         layer_name = layer_info.get('layer')
         layer = KerasLoader.__get_layer(layer_name)
 
@@ -53,6 +76,10 @@ class KerasLoader:
 
     @staticmethod
     def __get_layer(layer_code: str):
+        """
+        Returns the Keras layer class based on the provided layer code.
+        @param layer_code The code of the layer to return.
+        """
         if "dense_keras_layer" == layer_code:
             return Dense
         elif "lstm_keras_layer" == layer_code:
@@ -64,6 +91,10 @@ class KerasLoader:
 
     @staticmethod
     def __get_layer_params(layer_info: dict) -> dict:
+        """
+        Gets and returns the layer parameters based on the provided layer information.
+        @param layer_info Dictionary with information about the layer.
+        """
         layer_params = {}
 
         KerasLoader.__add_layer_param(layer_info, layer_params, 'input_shape')
@@ -74,6 +105,12 @@ class KerasLoader:
 
     @staticmethod
     def __add_layer_param(layer_info: dict, layer_params: dict, keyparam_to_add: str) -> None:
+        """
+        Adds a layer parameter to the layer_params dictionary if it exists in layer_info.
+        @param layer_info Dictionary with information about the layer.
+        @param layer_params Dictionary to store layer parameters.
+        @param keyparam_to_add Key parameter to add to layer_params.
+        """
         param_to_add = layer_info.get(keyparam_to_add, None)
         if param_to_add is not None:
             layer_params[keyparam_to_add] = param_to_add
