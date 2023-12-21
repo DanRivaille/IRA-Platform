ddc38a3374412a9a4a3468927dac1e497f586e31
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-21 11:27:44 -0300

    Se documenta el archivo History.py

--- src/algorithm/ml_model/History.py
+++ src/algorithm/ml_model/History.py
@@ -4,13 +4,26 @@ import numpy as np
 
 
 class History:
+    """
+    A class for saving the training history data of model execution.
+    """
     def __init__(self, train_loss: list, valid_loss: list, learning_rate: list, train_error_per_sample: np.ndarray):
+        """
+        Initializes an instance of History.
+        @param train_loss List of training losses for the model.
+        @param valid_loss List of validation losses for the model.
+        @param learning_rate List of learning rates during the training of the model.
+        @param train_error_per_sample An array containing the errors associated with each individual sample in the training set.
+        """
         self.train_loss = train_loss
         self.valid_loss = valid_loss
         self.learning_rate = learning_rate
         self.train_error_per_sample: np.ndarray = train_error_per_sample
 
     def to_json(self) -> dict:
+        """
+        Returns a dictionary for a .json file with the training history. 
+        """
         entries = {
             'train_loss': self.train_loss,
             'valid_loss': self.valid_loss,
@@ -20,6 +33,10 @@ class History:
         return entries
 
     def save(self, path: str):
+        """
+        Saves the training history to a .json file.
+        @param path The file path where the results will be saved.
+        """
         with open(path, 'w') as history_file:
             dump(self.to_json(), history_file)
 
