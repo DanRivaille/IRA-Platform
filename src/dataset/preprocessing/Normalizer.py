ad9e251a255d94ff005a2e4c1b98e42612d799d7
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2023-12-21 20:02:30 -0300

    Se documenta el archivo Normalizer.py

--- src/dataset/preprocessing/Normalizer.py
+++ src/dataset/preprocessing/Normalizer.py
@@ -9,12 +9,24 @@ from src.dataset.preprocessing.PreprocessStep import PreprocessStep
 
 
 class Normalizer(PreprocessStep):
+    """
+    A preprocess step that applies Min-Max normalization to the data in an IRADataset.
+    """
+
     __scaler: MinMaxScaler | None = None
 
     def __init__(self, new_range: tuple):
+        """
+        Initializes an instance of Normalizer.
+        @param new_range The desired range for the normalized values.
+        """
         self.__new_range = new_range
 
     def apply(self, dataset: IRADataset):
+        """
+        Applies Min-Max normalization to the data in the given IRADataset.
+        @param dataset A class for loading the datasets used by models.
+        """
         data = dataset.data
         original_shape = data.shape
         data_to_transform = data.reshape((-1, 1))
@@ -26,14 +38,25 @@ class Normalizer(PreprocessStep):
         dataset.data = Normalizer.__scaler.transform(data_to_transform).reshape(original_shape)
 
     def save(self, folder: str):
+        """
+        Saves the preprocess step to a specified folder.
+        @param folder The folder path where the preprocess step should be saved.
+        """
         file_path = os.path.join(folder, Normalizer.get_filename())
         joblib.dump(Normalizer.__scaler, file_path)
 
     def load(self, folder: str):
+        """
+        Loads the preprocess step from a specified folder.
+        @param folder The folder path from which the preprocess step should be loaded.
+        """
         file_path = os.path.join(folder, Normalizer.get_filename())
         if os.path.isfile(file_path):
             Normalizer.__scaler = joblib.load(file_path)
 
     @staticmethod
     def get_filename() -> str:
+        """
+        Returns the filename associated with the preprocess step.
+        """
         return 'normalizer.gz'
